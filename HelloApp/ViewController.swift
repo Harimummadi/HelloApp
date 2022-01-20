20604bd79db7af600a6b2658eaadbccd031b34ba
Hari Hara Mummadi <mummadiharihara9@gmail.com>
2022-01-20 12:16:03 -0600

    Update ViewController.swift

--- HelloApp/ViewController.swift
+++ HelloApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameOutlet: UITextField!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var gradeOutlet: UITextField!
+    
+    
+    
+    
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        // Read the data from the textbox and store it in a variable
+        var name = nameOutlet.text!
+        var grade = gradeOutlet.text!
+        
+        // Change the display label with this format. Hello, name!
+        displayLabel.text = "Hello,\(name)! Your Grade is \(grade)!"
+        
+    
+    
+    }
+    
+    
 
 }
 
